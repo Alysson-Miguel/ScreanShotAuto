5236e4cb7dc339f9f93b0c5a688ff717fc67006b
Alysson Miguel <Edicao.Dash@gmail.com>
2025-06-03 10:32:30 -0300

    Falha na adição do Bot

--- BotSea/Plugins.py/Echo.py
+++
@@ -1,8 +0,0 @@
-import cs_bot
-from cs_bot import MessageSession, logger
-from cs_bot.permissions import ANYONE
-
-@cs_bot.on_prefix(["echo"], permission=ANYONE)
-def echo(session: MessageSession):
-    logger.info(f"Mensagem recebida: {session.message.content}")
-    session.send(session.sender.email, session.message.content.lstrip("echo"))
